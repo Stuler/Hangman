1aabefa24ca7b9c75c5c3fc518f3d4a686f10039
Stuler <peter.jurek@gmail.com>
2020-08-28 10:25:28 +0200

    recode

--- hangman.py
+++ hangman.py
@@ -11,23 +11,17 @@ mod_mess = ""
 
 choices_count = 6
 
-while choices_count >0:
+while choices_count > 0:
     choice = input("Guess the letter: ")
     if choice in wrd_lwr:
         print (f"Letter '{choice}' is contained in the word.")
-    #    choice_pos = wrd_lwr.index(choice)
-    else:
-        print (f"Letter '{choice}' isn't contained in the word.")
+        mes = msg.replace("*", choice)
+        print(mes, "\n")
+
+else:
+    print (f"Letter '{choice}' isn't contained in the word.")
     choices_count = choices_count-1
-    for i in wrd_lwr:
-        if i == choice:
-            choice_pos = wrd_lwr.index(choice)
-            out_mess = wrd_lwr.replace("*", choice)
-            mod_mess = msg[:choice_pos] + choice + msg[choice_pos+1:]
-            msg = mod_mess 
-            print(msg)
-            print("")
-    
+
 guess = input("Guess the word: ")
 if guess == wrd_lwr:
     print("Right guess!")
