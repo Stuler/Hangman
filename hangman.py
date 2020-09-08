7e2c81f295bf55339c1bbcf5e69c6ee8a352a4ed
Stuler <peter.jurek@gmail.com>
2020-09-08 06:11:14 +0200

    basic game completed

--- hangman.py
+++ hangman.py
@@ -3,7 +3,7 @@ from random_word import RandomWords
 r = RandomWords()
 #wrd = r.get_random_word(minLength=5, maxLength=10)
 wrd = "Alaska"
-wrd_lwr = wrd.lower()
+wrd_lwr = wrd.lower().strip()
 print (wrd) # helper
 
 msg = "*"*len(wrd)
@@ -11,19 +11,30 @@ mod_mess = ""
 
 choices_count = 6
 choices_list = []
+unhid_word = []
+guessed_list = []
+init_word = ""
+
 
 while choices_count > 0:
     choice = input("Guess the letter: ")
     if choice in wrd_lwr:
-        print (f"Letter '{choice}' is contained in the word.")
+        print (f"Letter '{choice}' is contained in the word.\n")
         choices_list.append(choice)
     else:
         print (f"Letter '{choice}' isn't contained in the word.\n")
         choices_count = choices_count-1
     for i in wrd_lwr:
-        if i != choice:
-            mes = wrd_lwr.replace(i, "*")
-    print(mes,"\n")
+        if i in choices_list:
+            unhid_word.append(i)
+        else:
+            unhid_word.append('*')
+    
+    guessed = init_word.join(unhid_word)
+    guessed_list.append(guessed)
+    wrd_to_shw = guessed_list[-1][-len(wrd_lwr):]
+    print(wrd_to_shw, "\n")
+    print(f"{choices_count} guesses left!\n")
     
 
 guess = input("Guess the word: ")
--- test.py
+++ test.py
@@ -1,5 +1,21 @@
-wrd = "alaska"
+def get_indices(word, letter):
+    '''
+    Function returns list of guessed letters indices
+    '''
+    ind_lst = []
+    for i in word:
+        if i == letter:
+            ind = word.index(i)
+            ind_lst.append(ind)
+    return ind_lst
 
-wrd_rpl = wrd.replace("a", "*")
-
-print(wrd_rpl)
+def star_replacement(word, letter):
+    mod_msg = "*" * len(word)
+    for i in word:
+        if i == letter:
+            let_ind = word.index(i)
+            mod_msg[let_ind] = letter
+    return mod_msg
+            
+#print(get_indices("alaska","a"))
+print(star_replacement("alaska","s"))
